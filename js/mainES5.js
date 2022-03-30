c1a00f2bee568d6577d1f042c968bddec4e11815
sigrom <sigrom@gmail.com>
2022-03-30 16:25:00 +0100

    maj

--- js/mainES5.js
+++ js/mainES5.js
@@ -5,8 +5,8 @@ function Meme() {
         this.id=undefined;
         this.name='';
         this.texte='';
-        this.x=0;
-        this.y=0;
+        this.x=100;
+        this.y=100;
         this.fontSize=12;
         this.fontWeight='900'; 
         this.underline=false;
@@ -20,15 +20,16 @@ function Meme() {
             this.name='Dummy Demo Name';
             this.texte='Hello';
             this.x=50;
-            this.y=50;
-            this.fontSize=20;
+            this.y=200;
+            this.fontSize=50;
             this.fontWeight='100'; 
             this.underline=true;
             this.italic=true;
-            this.color='cadetblue';
+            this.color='#AFBC09';
             this.image='Futurama-6.jpg';
         }
 }
 // l'objet ne peux pas bouger
 var unMemeGlogal = Object.seal(new Meme());
 
+unMemeGlogal.SetDummy();
